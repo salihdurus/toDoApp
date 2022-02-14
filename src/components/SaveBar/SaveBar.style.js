3dce98c9dde4b7b46c8bd1023b4cdbc254370e1e
Salih Dürüs <durussalih@gmail.com>
2022-02-14 14:53:59 +0300

    initial commit

---
+++ src/components/SaveBar/SaveBar.style.js
@@ -0,0 +1,33 @@
+import {StyleSheet} from 'react-native';
+export default StyleSheet.create({
+  container: {
+    backgroundColor: '#36474f',
+    padding: 10,
+    margin: 15,
+    borderRadius: 10,
+  },
+  input: {
+    marginBottom: 10,
+    height: 38,
+    color: 'white',
+    borderBottomWidth: 1,
+    borderBottomColor: '#728791',
+    fontSize: 16,
+  },
+
+  button_null: {
+    alignItems: 'center',
+    backgroundColor: '#808080',
+    height: 40,
+    justifyContent: 'center',
+    borderRadius: 10,
+  },
+  button_text: {color: 'white'},
+  button: {
+    alignItems: 'center',
+    backgroundColor: '#fea500',
+    height: 40,
+    justifyContent: 'center',
+    borderRadius: 10,
+  },
+});
